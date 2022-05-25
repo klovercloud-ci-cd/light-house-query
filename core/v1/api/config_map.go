3a60395df600eab27905c011aff2d8b4b8346919
niloydeb1 <niloymishu@gmail.com>
2022-05-25 12:48:10 +0600

    Added GetByOwnerReference in ConfigMap api

--- core/v1/api/config_map.go
+++ core/v1/api/config_map.go
@@ -5,4 +5,5 @@ import "github.com/labstack/echo/v4"
 // ConfigMap api operations
 type ConfigMap interface {
 	Get(context echo.Context) error
+	GetByOwnerReference(context echo.Context) error
 }
--- core/v1/logic/config_map.go
+++ core/v1/logic/config_map.go
@@ -14,6 +14,10 @@ func (c configMapService) Get(agent string, option v1.ResourceQueryOption) ([]v1
 	return c.configMapRepo.Get(agent, option)
 }
 
+func (c configMapService) GetByOwnerReference(agent, ownerReference string, option v1.ResourceQueryOption) ([]v1.ConfigMap, int64) {
+	return c.configMapRepo.GetByOwnerReference(agent, ownerReference, option)
+}
+
 // NewConfigMapService returns service.ConfigMap type service
 func NewConfigMapService(configMapRepo repository.ConfigMap) service.ConfigMap {
 	return &configMapService{
