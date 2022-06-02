16b56568ac5b12ff10772c21ee11215823f6097c
RakibulHuda <rakibul.huda@klovercloud.com>
2022-06-02 15:54:00 +0600

    refactor network policy API

--- core/v1/logic/network_policy.go
+++ core/v1/logic/network_policy.go
@@ -10,12 +10,8 @@ type networkPolicyService struct {
 	networkPolicyRepo repository.NetworkPolicy
 }
 
-func (n networkPolicyService) Get(agent string, option v1.ResourceQueryOption) ([]v1.NetworkPolicy, int64) {
-	return n.networkPolicyRepo.Get(agent, option)
-}
-
-func (n networkPolicyService) GetByOwnerReference(agent, ownerReference string, option v1.ResourceQueryOption) ([]v1.NetworkPolicy, int64) {
-	return n.networkPolicyRepo.GetByOwnerReference(agent, ownerReference, option)
+func (n networkPolicyService) Get(agent, ownerReference, processId string, option v1.ResourceQueryOption) ([]v1.NetworkPolicy, int64) {
+	return n.networkPolicyRepo.Get(agent, ownerReference, processId, option)
 }
 
 // NewNetworkPolicyService returns service.NetworkPolicy type service
--- repository/v1/mongo/network_policy.go
+++ repository/v1/mongo/network_policy.go
@@ -19,47 +19,13 @@ type networkPolicyRepository struct {
 	timeout time.Duration
 }
 
-func (n networkPolicyRepository) Get(agent string, option v1.ResourceQueryOption) ([]v1.NetworkPolicy, int64) {
-	var results []v1.NetworkPolicy
-	query := bson.M{
-		"$and": []bson.M{{"agent_name": agent}},
-	}
-	coll := n.manager.Db.Collection(NetworkPolicyCollection)
-	skip := option.Pagination.Page * option.Pagination.Limit
-	findOptions := options.FindOptions{
-		Limit: &option.Pagination.Limit,
-		Skip:  &skip,
-		Sort:  bson.M{"created_at": -1},
-	}
-	if option.AscendingSort {
-		findOptions.Sort = bson.M{"created_at": 1}
-	}
-	result, err := coll.Find(n.manager.Ctx, query, &findOptions)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	for result.Next(context.TODO()) {
-		elemValue := new(v1.NetworkPolicy)
-		err := result.Decode(elemValue)
-		if err != nil {
-			log.Println("[ERROR]", err)
-			break
-		}
-		results = append(results, *elemValue)
-	}
-	count, err := coll.CountDocuments(n.manager.Ctx, query)
-	if err != nil {
-		log.Println(err.Error())
-	}
-	return results, count
-}
-
-func (n networkPolicyRepository) GetByOwnerReference(agent, ownerReference string, option v1.ResourceQueryOption) ([]v1.NetworkPolicy, int64) {
+func (n networkPolicyRepository) Get(agent, ownerReference, processId string, option v1.ResourceQueryOption) ([]v1.NetworkPolicy, int64) {
 	var results []v1.NetworkPolicy
 	query := bson.M{
 		"$and": []bson.M{
 			{"agent_name": agent},
 			{"obj.metadata.uid": ownerReference},
+			{"obj.metadata.labels.process_id": processId},
 		},
 	}
 	coll := n.manager.Db.Collection(NetworkPolicyCollection)
