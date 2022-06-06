0bb0f8d679b13b2328c3b6170765fe9b3862878b
niloydeb1 <niloymishu@gmail.com>
2022-06-06 16:08:22 +0600

    Added GetByAgentAndProcessIdAndOwnerReference and GetByAgentAndProcessId in repo layer of daemonSets

--- core/v1/repository/daemon_set.go
+++ core/v1/repository/daemon_set.go
@@ -4,5 +4,6 @@ import v1 "github.com/klovercloud-ci-cd/light-house-query/core/v1"
 
 // DaemonSet Repository operations.
 type DaemonSet interface {
-	Get(agent, ownerReference, processId string, option v1.ResourceQueryOption) ([]v1.DaemonSet, int64)
+	GetByAgentAndProcessId(agent, processId string, option v1.ResourceQueryOption) ([]v1.DaemonSet, int64)
+	GetByAgentAndProcessIdAndOwnerReference(agent, ownerReference, processId string, option v1.ResourceQueryOption) ([]v1.DaemonSet, int64)
 }
